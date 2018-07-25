e348ee3e52663e8398ae6a7256f6d708728242ac
alexyelis <alexyelis@gmail.com>
2018-07-25 10:36:41 +0300

    Update Point.java

--- Point.java
+++ Point.java
@@ -19,6 +19,15 @@ public class Point {
     public double getY() {
         return y;
     }
+   
+    public void setX(double x) {         
+        this.x = x;                      
+    }                                    
+                                         
+    public void setY(double y) {         
+        this.y = y;                      
+    }                                    
+                                         
 
     @Override
     public String toString() {
