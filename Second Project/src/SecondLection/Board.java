509340856ddb82ecd1d8e5b2a5bb9973cf152421
Oleksandr Yelisieiev <alexyelis@gmail.com>
2018-07-25 19:26:57 +0300

    commit

--- Second Project/src/SecondLection/Board.java
+++ Second Project/src/SecondLection/Board.java
@@ -47,7 +47,7 @@ public class Board {
         double sumArea = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] != null && arr[i].getArea() != 0) {
-                //sumArea = arr[i].getArea() + sumArea;
+                sumArea = arr[i].getArea() + sumArea;
             }
         }
         return sumArea;
