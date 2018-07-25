4d7d1e057e70d1bf07462f7be0acd430ce2dfb9b
alexyelis <alexyelis@gmail.com>
2018-07-25 10:43:09 +0300

    Update Square.java

--- Square.java
+++ Square.java
@@ -28,28 +28,28 @@ public class Square extends Shape {
         return p1;
     }
 
-    public Point getP2() {
-        return p2;
-    }
-
-    public Point getP3() {
-        return p3;
-    }
-
-    public Point getP4() {
-        return p4;
-    }
-
     public void setP1(Point p1) {
         this.p1 = p1;
+    }
+
+    public Point getP2() {
+        return p2;
     }
 
     public void setP2(Point p2) {
         this.p2 = p2;
     }
 
+    public Point getP3() {
+        return p3;
+    }
+
     public void setP3(Point p3) {
         this.p3 = p3;
+    }
+
+    public Point getP4() {
+        return p4;
     }
 
     public void setP4(Point p4) {
